705b76ea092710ef2bd3e1c37b0fbec17f18087d
holasoyender <javier.projectzafire@gmail.com>
2022-08-03 15:01:42 +0200

    cache port for production
    Co-Authored-By: Microondas <63871714+MicroondasV2@users.noreply.github.com>

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "flag"
 
 func main() {
-	port := flag.String("port", "9898", "Port to run cache server on")
+	port := flag.String("port", "9800", "Port to run cache server on")
 	maxMemory := flag.Int64("mem", 1073741824, "Max memory limit for the cache server")
 	flag.Parse()
 	srv, _ := NewServer(*port, *maxMemory)
